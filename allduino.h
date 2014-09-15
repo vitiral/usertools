9266338af247cc34f9ef1429a46a441f7feae0bd
Garrett Berg <gberg@neutral.io>
2014-09-15 09:21:15 -0600

    added hb and timesince

--- allduino.h
+++ allduino.h
@@ -22,6 +22,20 @@
  #define LED_PIN 13
 #endif
 
+#define TIME_SINCE(time) ((uint16_t)(millis() - (time)))
+
+#define HEARTBEAT() \
+    static uint16_t _heartbeat = millis(); \
+    static uint8_t _hb_led = 2; \
+    if(TIME_SINCE(_heartbeat) > 1000){ \
+        if(_hb_led == 2){ \
+            pinMode(LED_PIN, OUTPUT); \
+            _hb_led = 0; \
+        } \
+        digitalWrite(LED_PIN, _hb_led); \
+        _hb_led = 1 - _hb_led; \
+    }
+
 #define RUN_ONLY_EVERY(time) \
     static uint16_t start = millis(); \
     if(((uint16_t)millis()) - start < time) \
